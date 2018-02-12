fb55b43960e6d684f05c178a321cb2bda7381e5d
David Ly <d.ly@mail.utoronto.ca>
2018-02-11 21:13:19 -0500

    Added 29 feat comments

--- a1_extractFeatures.py
+++ a1_extractFeatures.py
@@ -29,9 +29,47 @@ def extract1( comment ):
     Returns:
         feats : numpy Array, a 173-length vector of floating point features (only the first 29 are expected to be filled, here)
     '''
-    print('TODO')
+
     # TODO: your code here
     feat_row = np.zeros(173)
+
+    # 1. Number of first-person pronouns
+
+    # 2. Number of second-person pronouns
+
+    # 3. Number of third-person pronouns
+    # 4. Number of coordinating conjunctions
+    # 5. Number of past-tense verbs
+    # 6. Number of future-tense verbs
+    # 7. Number of commas
+    # 8. Number of multi-character punctuation tokens
+    # 9. Number of common nouns
+    # 10. Number of proper nouns
+    # 11. Number of adverbs
+    # 12. Number of wh-words
+    # 13. Number of slang acronyms
+    # 14. Number of words in uppercase(>= 3 letters long)
+    # 15. Average length of sentences, in tokens
+    # 16. Average length of tokens, excluding punctuation - only tokens, in characters
+    # 17. Number of sentences.
+
+    ### Bristol, Gilhooly, and Logie norms ###
+
+    # 18. Average of AoA(100 - 700) from Bristol, Gilhooly, and Logie norms
+    # 19. Average of IMG from Bristol, Gilhooly, and Logie norms
+    # 20. Average of FAM from Bristol, Gilhooly, and Logie norms
+    # 21. Standard deviation of AoA(100 - 700) from Bristol, Gilhooly, and Logie norms
+    # 22. Standard deviation of IMG from Bristol, Gilhooly, and Logie norms
+    # 23. Standard deviation of FAM from Bristol, Gilhooly, and Logie norms
+
+    ### Warringer norms ###
+
+    # 24. Average of V.Mean.Sum from Warringer norms
+    # 25. Average of A.Mean.Sum from Warringer norms
+    # 26. Average of D.Mean.Sum from Warringer norms
+    # 27. Standard deviation of V.Mean.Sum from Warringer norms
+    # 28. Standard deviation of A.Mean.Sum from Warringer norms
+    # 29. Standard deviation of D.Mean.Sum from Warringer norms
 
     return feat_row
 
@@ -49,7 +87,7 @@ def main(args):
         comment = line['body']
         # the first 29 features
         feats[i, 0:173] = extract1(comment)  # first 173, everything after 29th are still zeros
-        print(feats[i])
+        #print(feats[i])
 
         # feat 30-173
 
